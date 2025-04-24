680e2c558e4d3bb4530871c36e9ef188d153cce7
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-24 23:40:54 +0330

    feat: add test commit

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log(bootstrap.name)
+
   app.listen(3000 , () => console.log("Server running on port 3000"))
 }
 bootstrap();
