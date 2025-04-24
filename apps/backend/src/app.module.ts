42957102c3d2e51b6f9a5a448f93ef518922d5d3
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-24 16:22:55 +0330

    feat: add backend modules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {BasketModule} from "./modules/basket/basket.module";
+import {DiscountModule} from "./modules/discount/discount.module";
+import {ProductModule} from "./modules/product/product.module";
+import {PaymentModule} from "./modules/payment/payment.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "akshaykuomar",
+      database: "postgres",
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [
+        "dist/**/**/**/*.entity{.ts,.js}",
+        "dist/**/**/*.entity{.ts,.js}",
+      ],
+    }),
+    ProductModule,
+    DiscountModule,
+    BasketModule,
+    PaymentModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
