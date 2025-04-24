25f51685b54e340cc29b194a2fb4161b03dc487e
MostafaMemari <m.memari711@gmail.com>
2025-04-24 15:37:19 +0330

    refactor: Integrate mock data for LatestBlogs component
    - Replaced hardcoded blog data in LatestBlogs component with imported mock data from BlogCarousels.
    - Created a new BlogCarousels file to store the latest blog entries, enhancing maintainability and consistency.

--- apps/frontend/src/components/templates/index/LatestBlogs/index.tsx
+++ apps/frontend/src/components/templates/index/LatestBlogs/index.tsx
@@ -1,40 +1,10 @@
 import CarouselBlog from "@/components/ui/CarouselBlog";
+import { latestBlogs } from "@/mock/BlogCarousels";
 
 const LatestBlogs = () => {
-  const blogs = [
-    {
-      id: 1,
-      imageSrc: "/images/blog/blog1.jpg",
-      title: "مزایا و معایب گوشی‌های سری گلکسی S21",
-      date: "29 / شهریور / 1403",
-      link: "/blog-detail",
-    },
-    {
-      id: 2,
-      imageSrc: "/images/blog/blog1.jpg",
-      title: "مزایا و معایب گوشی‌های سری گلکسی S21",
-      date: "29 / شهریور / 1403",
-      link: "/blog-detail",
-    },
-    {
-      id: 3,
-      imageSrc: "/images/blog/blog1.jpg",
-      title: "مزایا و معایب گوشی‌های سری گلکسی S21",
-      date: "29 / شهریور / 1403",
-      link: "/blog-detail",
-    },
-    {
-      id: 4,
-      imageSrc: "/images/blog/blog1.jpg",
-      title: "مزایا و معایب گوشی‌های سری گلکسی S21",
-      date: "29 / شهریور / 1403",
-      link: "/blog-detail",
-    },
-  ];
-
   return (
     <div>
-      <CarouselBlog sectionTitle="مطالب خواندنی" viewAllLink="/blog" viewAllText="مشاهده همه" blogs={blogs} />
+      <CarouselBlog sectionTitle="مطالب خواندنی" viewAllLink="/blog" viewAllText="مشاهده همه" blogs={latestBlogs} />
     </div>
   );
 };
