9efc2bcadcdffe00c3ee326be886a7e1c3458d03
MostafaMemari <m.memari711@gmail.com>
2025-04-24 14:52:18 +0330

    refactor: Rename Banner component to BannerSlider and implement dynamic banner data
    - Updated the Banner component to be renamed as BannerSlider for clarity.
    - Refactored the BannerSlider to utilize dynamic data for main and side banners, enhancing maintainability and flexibility.
    - Improved import statements for consistency across the application.

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Banner from "@/components/templates/index/BannerSlider";
+import BannerSlider from "@/components/templates/index/BannerSlider";
 import BestSellingProducts from "@/components/templates/index/BestSellingProducts";
 import CategoryBanners from "@/components/templates/index/CategoryBanners";
 import CategoryCircles from "@/components/templates/index/CategoryCircles";
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <div className="fixed inset-x-0 top-1/3 mx-auto h-1/3 w-1/4 bg-primary/50 blur-[200px]"></div>
 
-      <Banner />
+      <BannerSlider />
       <LatestProducts />
       <PopularProducts />
       <CategoryBanners />
