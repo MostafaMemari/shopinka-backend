1dee982b7a8d94b99a4b8c8f8b36f72e5da7887a
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-25 11:11:46 +0330

    feat: config redis and add config module

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { UserModule } from "../user/user.module";
+import { RedisModule } from "@nestjs-modules/ioredis";
+import { redisConfig } from "src/configs/redis.config";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [ConfigModule.forRoot({ envFilePath: "./.env" }), RedisModule.forRoot(redisConfig()), AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
