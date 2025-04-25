69350a53a3be726640a02ba5a75028dbab1527a5
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-04-25 20:06:29 +0330

    feat: add auth guard

---
+++ apps/backend/src/common/guards/role.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "../decorators/role.decorator";
+import { Request } from "express";
+import { Role } from "generated/prisma";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) { }
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const roles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler())
+
+        if (!roles) return true
+
+        const req: Request = context.switchToHttp().getRequest()
+
+        return roles.includes(req.user.role)
+    }
+}
