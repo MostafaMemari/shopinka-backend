2fd08016ca1ba2382a771b848c1842caf777b053
MostafaMemari <m.memari711@gmail.com>
2025-04-29 13:58:59 +0330

    refactor: enhance profile and order management components for better usability
    - Updated the Profile component to improve data handling and user experience.
    - Refactored OrdersPage to better integrate with the new order status configuration.
    - Streamlined the import paths for profile-related components to enhance maintainability.
    - Removed outdated components to simplify the codebase and improve overall organization.

---
+++ apps/frontend/src/components/shop/Accordion.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { FC, useState } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+
+interface AccordionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Accordion: FC<AccordionProps> = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <li className="relative" data-accordion-item>
+      <div className="w-full text-right cursor-pointer" data-accordion-button onClick={() => setIsOpen(!isOpen)}>
+        <div className="flex items-center justify-between gap-2 text-sm md:text-base">
+          <span>{title}</span>
+          <div className="min-w-fit">
+            <FiChevronLeft className={`h-5 w-5 duration-300 ${isOpen ? "rotate-90" : ""}`} />
+          </div>
+        </div>
+      </div>
+      <div className={`relative overflow-hidden transition-all duration-300 ${isOpen ? "max-h-60" : "max-h-0"}`} data-accordion-content>
+        <div className="mt-4 max-h-60 overflow-y-auto pl-1">{children}</div>
+      </div>
+    </li>
+  );
+};
+
+export default Accordion;
---
+++ apps/frontend/src/components/shop/MobileOptions.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { FC } from "react";
+import { FiFilter } from "react-icons/fi";
+import { BsSortDown } from "react-icons/bs";
+
+interface MobileOptionsProps {
+  onFilterClick: () => void;
+  onSortClick: () => void;
+}
+
+const MobileOptions: FC<MobileOptionsProps> = ({ onFilterClick, onSortClick }) => {
+  return (
+    <div className="mb-6 flex items-center justify-center gap-x-4 md:hidden">
+      <button
+        className="flex w-full items-center gap-x-4 rounded-lg bg-muted px-4 py-3 text-sm xs:text-base"
+        onClick={onFilterClick}
+        aria-controls="shop-filter-drawer-navigation"
+        type="button"
+      >
+        <FiFilter className="h-6 w-6" />
+        <div>فیلتر</div>
+      </button>
+      <button
+        className="flex w-full items-center gap-x-4 rounded-lg bg-muted px-4 py-3 text-sm xs:text-base"
+        onClick={onSortClick}
+        aria-controls="shop-sort-drawer-navigation"
+        type="button"
+      >
+        <BsSortDown className="h-6 w-6" />
+        <div>مرتب سازی</div>
+      </button>
+    </div>
+  );
+};
+
+export default MobileOptions;
