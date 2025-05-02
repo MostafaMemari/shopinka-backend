288799643aef5d5d20402ff59e4ad5c34aa3d790
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-05-02 17:50:17 +0330

    feat: add product variant repository and product variant messages enum

---
+++ backend/src/modules/product/enums/product-variant-messages.enum.ts
@@ -0,0 +1,7 @@
+export enum ProductVariantMessages {
+    AlreadyExistsProductVariant = 'ProductVariant with this sku already exists.',
+    NotFoundProductVariant = 'ProductVariant not found',
+    RemovedProductVariantSuccess = 'ProductVariant removed successfully',
+    CreatedProductVariantSuccess = 'ProductVariant created successfully',
+    UpdatedProductVariantSuccess = 'ProductVariant Updated successfully.',
+}
