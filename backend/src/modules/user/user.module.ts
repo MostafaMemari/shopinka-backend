1b22cad5edac291a628fd51a6cfb62d2bad744de
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-05-04 16:54:53 +0330

    feat: add find all favorites method

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -5,10 +5,11 @@ import { PrismaModule } from "../prisma/prisma.module";
 import { UserRepository } from "./user.repository";
 import { AuthService } from "../auth/auth.service";
 import { CacheModule } from "../cache/cache.module";
+import { FavoriteRepository } from "../product/repositories/favorite.repository";
 
 @Module({
-  imports: [PrismaModule , CacheModule],
+  imports: [PrismaModule, CacheModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository, AuthService],
+  providers: [UserService, UserRepository, AuthService, FavoriteRepository],
 })
 export class UserModule { }
