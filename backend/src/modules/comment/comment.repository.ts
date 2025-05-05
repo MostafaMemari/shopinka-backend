c1f98e350a6abd17f31b4a0b3f1b7af53bc1fd01
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-05-05 20:46:25 +0330

    feat: add comment repository

---
+++ backend/src/modules/comment/comment.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { Comment, Prisma } from "generated/prisma";
+import { CommentMessages } from "./enums/comment-messages.enum";
+
+@Injectable()
+export class CommentRepository {
+    constructor(private readonly prismaService: PrismaService) { }
+
+    create(args: Prisma.CommentCreateArgs): Promise<Comment> {
+        return this.prismaService.comment.create(args);
+    }
+
+    findAll(args: Prisma.CommentFindManyArgs = {}): Promise<Comment[]> {
+        return this.prismaService.comment.findMany(args)
+    }
+
+    findOne(args: Prisma.CommentFindFirstArgs): Promise<Comment | null> {
+        return this.prismaService.comment.findFirst(args)
+    }
+
+    update(args: Prisma.CommentUpdateArgs): Promise<Comment> {
+        return this.prismaService.comment.update(args)
+    }
+
+    delete(args: Prisma.CommentDeleteArgs): Promise<Comment> {
+        return this.prismaService.comment.delete(args)
+    }
+
+    async findOneOrThrow(args: Prisma.CommentFindFirstArgs): Promise<Comment | never> {
+        const comment = await this.findOne(args)
+
+        if (!comment) throw new NotFoundException(CommentMessages.NotFoundComment)
+
+        return comment
+    }
+}
