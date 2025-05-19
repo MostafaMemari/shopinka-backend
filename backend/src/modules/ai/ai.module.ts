284883498fec245702219105bfe4705fc62df1ec
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-05-19 21:10:59 +0330

    add user to seoMeta model

--- backend/src/modules/ai/ai.module.ts
+++ backend/src/modules/ai/ai.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { AiController } from './ai.controller';
+import { TagRepository } from '../tag/tag.repository';
 
 @Module({
   controllers: [AiController],
-  providers: [AiService],
+  providers: [AiService, TagRepository],
 })
 export class AiModule {}
