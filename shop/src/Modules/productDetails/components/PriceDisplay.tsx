f2488fc9bd520639951df3ef7ff10e0407bf46bf
MostafaMemari <m.memari711@gmail.com>
2025-06-03 13:14:33 +0330

    refactor: update PriceDisplay component for improved price handling
    - Removed commented out code for newPrice check to enhance clarity.
    - Updated discount display logic to show newPrice instead of oldPrice when applicable.
    - Adjusted rendering of price elements to ensure correct pricing information is displayed.

--- shop/src/Modules/productDetails/components/PriceDisplay.tsx
+++ shop/src/Modules/productDetails/components/PriceDisplay.tsx
@@ -17,17 +17,19 @@ export function ProductPrice() {
     return 0;
   }, [newPrice, oldPrice]);
 
+  console.log(discount);
+
   if (!product) return null;
   if (product.type === 'VARIABLE' && !selectedVariant) return null;
-  if (!newPrice) return null;
+  // if (!newPrice) return null;
 
   return (
     <div className="space-y-1" aria-live="polite">
       {discount > 0 && (
         <div className="flex items-center gap-x-2">
-          {oldPrice && (
+          {newPrice && (
             <div>
-              <del className="text-xs text-text/60 decoration-warning md:text-sm">{formatPrice(oldPrice)}</del>
+              <del className="text-xs text-text/60 decoration-warning md:text-sm">{formatPrice(newPrice)}</del>
             </div>
           )}
           <div className="flex w-8 items-center justify-center rounded-full bg-warning py-0.5 text-xs font-bold text-white dark:bg-red-600">
@@ -36,7 +38,7 @@ export function ProductPrice() {
         </div>
       )}
       <div className="text-primary">
-        <span className="text-base font-semibold lg:text-lg lg:font-bold">{formatPrice(newPrice)}</span>
+        <span className="text-base font-semibold lg:text-lg lg:font-bold">{formatPrice(oldPrice)}</span>
         <span className="text-xs font-light lg:text-sm lg:font-medium"> تومان</span>
       </div>
     </div>
