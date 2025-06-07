7a5f46ef1742451bd1b7b0b19e857abfb847b68e
MostafaMemari <m.memari711@gmail.com>
2025-06-07 14:11:22 +0330

    refactor: simplify checkout page by integrating CheckoutPageView
    - Removed AddressSection, DeliverySection, and CartPriceDetail components from the checkout page.
    - Integrated CheckoutPageView to streamline the layout and improve code maintainability.

---
+++ shop/src/Modules/checkout/views/CheckoutPageView.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AddressSection from '../components/AddressSection';
+import DeliverySection from '../components/DeliverySection';
+import CartPriceDetail from '../components/CartPriceDetail';
+
+function CheckoutPageView() {
+  return (
+    <>
+      <div className="col-span-12 md:col-span-8">
+        <div className="rounded-lg bg-muted p-4">
+          <AddressSection />
+          <DeliverySection />
+        </div>
+      </div>
+      <CartPriceDetail />
+    </>
+  );
+}
+
+export default CheckoutPageView;
--- shop/src/app/(cart)/checkout/page.tsx
+++ shop/src/app/(cart)/checkout/page.tsx
@@ -1,19 +1,11 @@
-import AddressSection from '@/Modules/checkout/components/AddressSection';
-import CartPriceDetail from '@/Modules/checkout/components/CartPriceDetail';
 import CheckoutProgress from '@/Modules/checkout/components/CheckoutProgress';
-import DeliverySection from '@/Modules/checkout/components/DeliverySection';
+import CheckoutPageView from '@/Modules/checkout/views/CheckoutPageView';
 
 export default function Page() {
   return (
     <>
       <CheckoutProgress currentStep="checkout" />
-      <div className="col-span-12 md:col-span-8">
-        <div className="rounded-lg bg-muted p-4">
-          <AddressSection />
-          <DeliverySection />
-        </div>
-      </div>
-      <CartPriceDetail />
+      <CheckoutPageView />
     </>
   );
 }
