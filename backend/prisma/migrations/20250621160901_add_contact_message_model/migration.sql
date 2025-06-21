5ce40bca96e7205ddfdf8c0d84a92a979b4ed1ba
MostafaMemari <m.memari711@gmail.com>
2025-06-21 19:39:25 +0330

    feat:  add contact message model

---
+++ backend/prisma/migrations/20250621160901_add_contact_message_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "ContactMessage" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactMessage_pkey" PRIMARY KEY ("id")
+);
